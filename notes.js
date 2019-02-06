3648779da526bf2d656fe6d084c7f0a8377fb7a8
Ryan Dulac <analyticrye@gmail.com>
2019-02-06 18:48:06 -0500

    project started with yarg and lodash

---
+++ notes.js
@@ -0,0 +1,21 @@
+console.log("Starting notes.js");
+
+const addNote = (title, body) => {
+  console.log("adding note", title, body);
+};
+const getAll = () => {
+  console.log("Getting all notes");
+};
+const getNote = title => {
+  console.log("Getting note", title);
+};
+const removeNote = title => {
+  console.log("Removing note", title);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+};
